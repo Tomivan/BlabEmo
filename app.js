c29a0af9f7f5911f358b2965063c662f24b93d36
Pheonix_King <bukunmi@ashnelson.com>
2021-10-28 23:44:45 +0100

    complete project

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+
+    let images = [
+        './assets/images/hero.png',
+        './assets/images/Daco.png',
+    ];
+
+    images.current = 0;
+
+    let img = document.getElementById( 'main-photo' );
+
+    setInterval(function () {
+        console.log("here")
+        img.src = images[ images.current++ ];
+        if ( images.current === images.length ) images.current = 0;       
+    }, 3000 );
+
+};
+
+count = 0;
+let quotes = [
+    'A life spent making mistakes is not only more <br /> honorable, but more useful than a life spent <br />doing nothing.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/> sed do eiusmod tempor incididunt ut labore et aliqua.<br /> Ut enim ad minim veniam.'
+]
+setInterval(function () {
+    count++;
+    $("#blockquote").fadeOut(300, function() {
+        $(this).html(quotes[count % quotes.length]).fadeIn(500);
+        console.log("blockquote")
+    });
+}, 3000);
